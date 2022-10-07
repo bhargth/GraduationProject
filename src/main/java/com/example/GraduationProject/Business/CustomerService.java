5ec7cdfe7ffe26c7674337f4dc71a05d9eca5d3b
Bharghavi Thummalasetty <bharghavi11@gmail.com>
2022-10-07 17:20:35 +0200

    seventhcommit

--- src/main/java/com/example/GraduationProject/Business/CustomerService.java
+++ src/main/java/com/example/GraduationProject/Business/CustomerService.java
@@ -1,7 +1,9 @@
 package com.example.GraduationProject.Business;
 
 import com.example.GraduationProject.Business.Entity.Customer;
+import com.example.GraduationProject.Business.Entity.Product;
 import com.example.GraduationProject.RepositoryLayer.CustomerRepository;
+import com.example.GraduationProject.RepositoryLayer.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
@@ -16,6 +18,9 @@ public class CustomerService {
 
     @Autowired
     CustomerRepository custRep;
+
+    @Autowired
+    ProductRepository prodRep;
 
     ShoppingCart cart = new ShoppingCart();
 
@@ -38,6 +43,13 @@ public class CustomerService {
 
     }
 
+    public void addToCart(long productId){
+        Product p= prodRep.findById(productId).get();
+        cart.addProduct(p);
+    }
 
+    public ShoppingCart getCart(){
+        return cart;
+    }
 
 }
--- src/main/java/com/example/GraduationProject/PresentationLayer/CustomerController.java
+++ src/main/java/com/example/GraduationProject/PresentationLayer/CustomerController.java
@@ -4,6 +4,7 @@ import com.example.GraduationProject.Business.CustomerService;
 import com.example.GraduationProject.Business.Entity.Customer;
 import com.example.GraduationProject.Business.Entity.Product;
 import com.example.GraduationProject.Business.ProductService;
+import com.example.GraduationProject.Business.ShoppingCart;
 import com.example.GraduationProject.RepositoryLayer.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -33,7 +34,7 @@ public class CustomerController {
 @PostMapping("/signupForm")
 public String signupSubmit(@ModelAttribute Customer customer, Model model){
 custser.signUp(customer);
-model.addAttribute("customer",customer);
+model.addAttribute("products", prodser.getProducts());
 return "products";
 }
 
@@ -62,6 +63,23 @@ return "products";
 
 
 
+    @GetMapping("/shoppingcart")
+    public String shoppingCart(Model model) {
+        model.addAttribute("cart", custser.getCart());
+        return  "ShoppingCart" ;
+    }
+    @PostMapping("/addtocart/{productId}")
+    public String addToCart(@PathVariable long productId, Model model){
+        custser.addToCart(productId);
+        model.addAttribute("products", prodser.getProducts());
+        return "products";
+    }
+
+
+
+
+
+
 
     @RequestMapping("/HowitWorks")
     public String howItWorks() { return  "HowitWorks" ;}
@@ -70,6 +88,7 @@ return "products";
     public String aboutUs() {
         return  "aboutusPage" ;
     }
+
 
 
 
